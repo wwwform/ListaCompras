fdb27aa3b6ffc25f6231dd3af51587499d18176b
wwwform <168577329+wwwform@users.noreply.github.com>
2025-05-02 16:26:20 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('compras-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/css/style.css',
+        '/script.js',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
